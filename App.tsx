391a73aacb6b5508b448ca4b720b5581047be1f2
Libin Geevarghese <lbngv7@gmail.com>
2024-03-20 02:31:04 +0530

    new ui base

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {StyleSheet, View, StatusBar, SafeAreaView} from 'react-native';
+import {StyleSheet, View, StatusBar, SafeAreaView, Text} from 'react-native';
 import ImageReader from './features/ImageReader';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <StatusBar barStyle="dark-content" backgroundColor="#f0f0f0" />
+      <Text style={styles.title}>Lets TextImg...</Text>
+      <Text style={styles.text}>Extract Text from Image</Text>
       <View style={styles.appContainer}>
         <ImageReader />
       </View>
@@ -17,6 +19,20 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
     backgroundColor: '#f0f0f0',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    fontFamily: 'monospace',
+    marginHorizontal: 10,
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+    fontFamily: 'monospace',
+    marginHorizontal: 10,
   },
   appContainer: {
     flex: 1,
--- features/ImageReader.tsx
+++ features/ImageReader.tsx
@@ -6,10 +6,15 @@ import {
   ActivityIndicator,
   StyleSheet,
   TouchableOpacity,
+  Dimensions,
+  ScrollView,
+  Alert,
 } from 'react-native';
 import {launchCamera} from 'react-native-image-picker';
 import TextRecognition from '@react-native-ml-kit/text-recognition';
+import Clipboard from '@react-native-clipboard/clipboard';
 
+const {width, height} = Dimensions.get('window');
 const ImageReader = () => {
   const [text, setText] = useState('');
   const [imageUri, setImageUri] = useState(null);
@@ -40,35 +45,57 @@ const ImageReader = () => {
     }
   };
 
+  const copyToClipBoard = () => {
+    Clipboard.setString(text);
+    Alert.alert('Copied', 'Text copied to clipboard');
+  };
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={handleTakePhoto}>
-        <Text style={styles.buttonText}>Take Photo</Text>
-      </TouchableOpacity>
       {loading && (
         <ActivityIndicator style={styles.loader} size="large" color="#1e90ff" />
       )}
       {imageUri && !loading && (
         <>
           <Image source={{uri: imageUri}} style={styles.image} />
-          <Text style={styles.text}>Detected Text: {text}</Text>
+          <View style={{height: 200, paddingVertical: 15}}>
+            <Text>Detected Text</Text>
+            <TouchableOpacity
+              onPress={copyToClipBoard}
+              style={{backgroundColor: 'blue', height: 50, width: 150}}>
+              <Text>COPY</Text>
+            </TouchableOpacity>
+            <ScrollView>
+              <Text style={styles.text}>{text}</Text>
+            </ScrollView>
+          </View>
         </>
       )}
+      <TouchableOpacity style={styles.button} onPress={handleTakePhoto}>
+        <Text style={styles.buttonText}>Take Photo</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    paddingBottom: 10,
+    marginTop: 50,
+    borderTopLeftRadius: 100,
+    borderTopRightRadius: 100,
+    width,
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#cad6e6',
   },
   button: {
+    width: 300,
+    alignItems: 'center',
     backgroundColor: '#1e90ff',
     padding: 15,
-    borderRadius: 8,
+    borderRadius: 30,
     marginBottom: 20,
   },
   buttonText: {
@@ -77,20 +104,20 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   image: {
-    width: 300,
-    height: 300,
+    width: 200,
+    height: 200,
     borderRadius: 10,
     marginVertical: 20,
   },
   text: {
+    flex: 1,
+    // height: 500,
+    width: width * 0.9,
     fontSize: 16,
     color: '#333',
     textAlign: 'center',
-    marginHorizontal: 20,
-    marginTop: 20,
     backgroundColor: '#fff',
-    padding: 10,
-    borderRadius: 5,
+    borderRadius: 10,
     elevation: 3,
   },
   loader: {
